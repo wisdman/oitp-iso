8a848caf97b65281f4eaea6267ef647ab38bcf63
Wisdman <wisdman@ajaw.it>
2019-07-28 07:57:26 +0500

    update big

--- frontends/app/src/components/indicator-brain/indicator-brain.component.ts
+++ frontends/app/src/components/indicator-brain/indicator-brain.component.ts
@@ -1,12 +1,13 @@
 import {
-  Component,
   ChangeDetectionStrategy,
+  Component,
   ElementRef,
-  ViewChild,
+  OnDestroy,
+  OnInit,
 } from "@angular/core"
 
-// import { Subscription } from "rxjs"
-// import { map } from "rxjs/operators"
+import { Subscription } from "rxjs"
+import { map } from "rxjs/operators"
 
 import { ProgressService } from "../../services"
 
@@ -16,30 +17,29 @@ import { ProgressService } from "../../services"
   styleUrls: [ "./indicator-brain.component.css" ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IndicatorBrainComponent {
-  @ViewChild("brainNode", { static: true }) brainRef?: ElementRef<HTMLDivElement>
-
-  constructor(private _progressService: ProgressService) {}
+export class IndicatorBrainComponent implements OnInit, OnDestroy {
+  constructor(
+    private _elRef:ElementRef<HTMLElement>,
+    private _progressService: ProgressService,
+  ) { }
   public progress = this._progressService.progress
 
+  private _progressSubscription?: Subscription
 
-  // private _userSubscription?: Subscription
+  ngOnInit() {
+    if (this._progressSubscription) this._progressSubscription.unsubscribe()
+    this._progressSubscription = this
+                                    ._progressService
+                                    .progress
+                                    .pipe(map(p => p.charge))
+                                    .subscribe(value => {
+                                      window.requestAnimationFrame(()=>{
+                                        this._elRef.nativeElement.style.setProperty("--value", `${value}`)
+                                      })
+                                    })
+  }
 
-  // ngOnInit() {
-  //   this._userSubscription = this.user
-  //                                 .pipe(map(user => user && user.charge || 0))
-  //                                 .subscribe(value => {
-  //                                   window.requestAnimationFrame(()=>{
-  //                                     if (this.brainRef && this.brainRef.nativeElement) {
-  //                                       this.brainRef.nativeElement.style.setProperty("--value", `${value}`)
-  //                                     }
-  //                                   })
-  //                                 })
-  // }
-
-  // ngOnDestroy() {
-  //   if (this._userSubscription) {
-  //     this._userSubscription.unsubscribe()
-  //   }
-  // }
+  ngOnDestroy() {
+    if (this._progressSubscription) this._progressSubscription.unsubscribe()
+  }
 }
